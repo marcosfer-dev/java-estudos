70c807301d03bf14edc3c9a362165bee410dccf3
Marcos Fernandes <fernandesmarcos.programador@gmail.com>
2026-08-01 14:07:20 -0300

    feat: adiciona prática de operadores aritmeticos

---
+++ src/marcosfer/dev/com/github/operadores/aritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,34 @@
+package marcosfer.dev.com.github.operadores.aritmeticos;
+
+import java.util.Scanner;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+            System.out.print("Nome: ");
+            String nome = sc.nextLine();
+
+            System.out.print("Idade: ");
+            int idadeAtual = sc.nextInt();
+            int idadeEm10 = idadeAtual + 10;
+
+            System.out.print("Salário: ");
+            double salarioMensal = sc.nextDouble();
+            double salarioAnual = salarioMensal * 12;
+
+            System.out.println("==========================");
+            System.out.println("RELATÓRIO");
+            System.out.println("==========================");
+            System.out.println("Nome: " + nome
+            + "\nIdade atual: " + idadeAtual
+            + "\nIdade em 10 anos: " + idadeEm10
+            + "\nSalário mensal: " + salarioMensal
+            + "\nSalário anual: " + salarioAnual);
+            System.out.println("==========================");
+
+
+        sc.close();
+    }
+}
